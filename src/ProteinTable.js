8ac84c8aca392557d7af16021622748d4ea555d1
Rashmi Baheti <baheti.rash123@gmail.com>
2023-12-16 13:10:15 -0800

    using lazyQuery for paginated data & bug fix
    - fetching data using useLazyQuery for paginated data
    -fix a bug in pagination next page

--- src/ProteinTable.js
+++ src/ProteinTable.js
@@ -1,57 +1,64 @@
 import React, { useState, useEffect } from "react";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, useLazyQuery } from "@apollo/client";
 import { LOAD_SAMPLES } from "./GraphQl/Queries";
 import { Table, Pagination, PaginationItem, PaginationLink } from "reactstrap";
 import "./ProteinTable.css";
 
 const ProteinTable = () => {
-  const { error, loading, data } = useQuery(LOAD_SAMPLES, {
-    variables: { first: 3, offset: 2 },
-  });
-  const [rowData, setRowData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+
+  // Calling "useQuery" only to get full-data for pagination's 2nd page.
+  // If we had a total count of data entries, then we wouldn't have needed to call this.
+  const {
+    error: fdError,
+    loading: fdLoading,
+    data: fullData,
+  } = useQuery(LOAD_SAMPLES, { variables: { first: 10000, offset: 0 } });
+  // first and offset variables are used here to know which elements to fetch from the GraphQl api.
+  const [getSamplesData, { loading, error, data }] = useLazyQuery(LOAD_SAMPLES);
+
   useEffect(() => {
-    console.log("data: ", data);
+    let first = 3;
+    let offset = (currentPage - 1) * 3;
+    getSamplesData({ variables: { first, offset } });
+  }, [currentPage]);
+
+  const getRowData = (data) => {
     if (data && data.allSamples && data.allSamples.nodes) {
-      setRowData(data.allSamples.nodes);
+      return data.allSamples.nodes;
     }
-  }, data);
+    return [];
+  };
+
+  useEffect(() => {
+    getSamplesData({ variables: { first: 3, offset: 0 } });
+  }, []);
+
   const changePage = (pageNumber) => {
     if (currentPage === pageNumber) return;
     setCurrentPage(pageNumber);
   };
+
   const onPageNumberClick = (pageNumber) => {
     changePage(pageNumber);
   };
+
   const onPreviousPageClick = () => {
     if (currentPage === 1) return;
     changePage((currentPage) => currentPage - 1);
   };
+
   const onNextPageClick = () => {
-    if (currentPage === Math.ceil(rowData.length / 3)) return;
+    if (currentPage === Math.ceil(getRowData(fullData).length / 3)) return;
     changePage((currentPage) => currentPage + 1);
   };
+
   const isCurrentPageFirst = currentPage === 1;
   const isCurrentPageLast = currentPage === 2;
-  const renderTablePage = () => {
-    if (rowData.length === 0) return null;
-    let end = 3 * currentPage;
-    let start = end - 3;
-    if (end > rowData.length) {
-      end = rowData.length;
-    }
-    const renderRows = [];
-    for (let i = start; i < end; i++) {
-      renderRows.push(
-        <tr>
-          <td>{rowData[i].proteinName}</td>
-          <td>{rowData[i].abundance}</td>
-          <td>{rowData[i].confidence}</td>
-        </tr>
-      );
-    }
-    return renderRows;
-  };
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error?.message || "Error occured!"}</div>;
+
   return (
     <Table size="sm">
       <thead>
@@ -62,31 +69,21 @@ const ProteinTable = () => {
         </tr>
       </thead>
       <tbody>
-        {renderTablePage()}
-        {/* {rowData.map((row, index) => {
-          if (currentPage === 1 && index < 3) {
-            return (
-              <tr>
-                <td>{row.proteinName}</td>
-                <td>{row.abundance}</td>
-                <td>{row.confidence}</td>
-              </tr>
-            );
-          } else {
-            return (
-              <tr>
-                <td>{row.proteinName}</td>
-                <td>{row.abundance}</td>
-                <td>{row.confidence}</td>
-              </tr>
-            );
-          }
-        })} */}
+        {getRowData(data).map((row) => {
+          return (
+            <tr>
+              <td>{row.proteinName}</td>
+              <td>{row.abundance}</td>
+              <td>{row.confidence}</td>
+            </tr>
+          );
+        })}
         <tr size="sm">
           <td></td>
           <td></td>
           <td className="d-flex justify-content-end m-0">
             <Pagination size="sm">
+              {/* using static array for pagination here to keep it simple. */}
               {[1, 2].map((pageNumber) => {
                 return (
                   <PaginationItem
